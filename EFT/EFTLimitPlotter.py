a6f5ea4345f20640ea3a442933bcbe9f7a762f5b
Ethan Lewis Simpson <ethansimpson285@gmail.com>
2021-10-27 17:31:39 +0200

    updated functionality

--- EFT/EFTLimitPlotter.py
+++ EFT/EFTLimitPlotter.py
@@ -23,29 +23,30 @@ Each tuple should be of the form (global mode, lower bound, upper bound)
 
 
 
-
-data = {"ctG":[(0,-1,1),(0,-3,3)],"ctZ":[(2,-3,8),None],"ctH":[(6,2,10),None]}
-
-# for tup_list in data.values():
-#     for tup in tup_list:
-#         for x in list(tup):
-#             print(x)
-#             input()
-#         # for x in list(data[wc][tup]):
-#         #     print(x)
-#         #     input()
-# # data = {"ctG":(())}
-
-df = pd.DataFrame.from_dict(data).T
-
-df.columns=["Linear","Linear + Quadratic"]
-
-
 class EFT_Plot:
 
-    def __init__(self,df,orientation):
+    """
+    Main EFT Plot Class
+    Takes a pandas DataFrame as input
+    """
+
+    def __init__(self,df,**kwargs):
+
         self.df = df
-        self.orientation = orientation
+        self.orientation = kwargs["orientation"] if "orientation" in kwargs else "vertical"
+        
+        # Assign which columns to plot
+        if "to_plot" in kwargs:
+            if kwargs["to_plot"]=="all": 
+                self.to_plot = self.df.columns.tolist()
+            if isinstance(kwargs["to_plot"],list) and all([x in self.df.columns.tolist() for x in kwargs["to_plot"]]):
+                self.to_plot = kwargs["to_plot"]
+        else:
+            self.to_plot = self.df.columns.tolist()
+
+        self.number2plot = len(self.to_plot)
+        self.generate_wc_ordinates()
+
 
     def get_points(self,column,index,row):
             wilson_coef=index
@@ -61,16 +62,34 @@ class EFT_Plot:
             return value,error_array
 
 
+    def generate_wc_ordinates(self):
+        assert self.number2plot < 4 and isinstance(self.number2plot,int), "Too many branches to plot"
+        if self.number2plot==1:
+            self.wc_array = [0]
+        if self.number2plot==2:
+            self.wc_array = [-0.05,0.05]
+        if self.number2plot==3:
+            self.wc_array = [-0.15,0,0.15]
+
+
+
+    def make_plot(self):
+        if self.orientation=="hor":
+            self.make_horizontal_plot()
+        elif self.orientation=="vert":
+            self.make_vertical_plot()
+
+
     def make_vertical_plot(self):
 
         fig, ax = plt.subplots()
 
         y_points = np.arange(0,df.shape[0]+1)
 
-        for i,(index,row) in enumerate(self.df.iterrows()):
-
-            value,asymmetric_error = self.get_points("Linear",index,row)
-            plt.errorbar(x=value,y=i+1,xerr=asymmetric_error,fmt='x')
+        for col,yp in zip(self.to_plot,self.wc_array):
+            for i,(index,row) in enumerate(self.df.iterrows()):
+                value,asymmetric_error = self.get_points(col,index,row)
+                plt.errorbar(x=value,y=i+1+yp,xerr=asymmetric_error,fmt='x')
 
         ax.set_yticks(y_points[1:])
         ax.set_yticklabels(self.df.index)
@@ -87,10 +106,12 @@ class EFT_Plot:
 
         x_points = np.arange(0,df.shape[0]+1)
 
-        for i,(index,row) in enumerate(self.df.iterrows()):
+        for col,xp in zip(self.to_plot,self.wc_array):
 
-            value,asymmetric_error = self.get_points("Linear",index,row)
-            plt.errorbar(x=i+1,y=value,yerr=asymmetric_error,fmt='x')
+            for i,(index,row) in enumerate(self.df.iterrows()):
+
+                value,asymmetric_error = self.get_points("Linear",index,row)
+                plt.errorbar(x=i+1+xp,y=value,yerr=asymmetric_error,fmt='x')
 
         ax.set_xticks(x_points[1:])
         ax.set_xticklabels(self.df.index)
@@ -105,17 +126,18 @@ class EFT_Plot:
 
 
 
-
 class LHC_EFT_Plot(EFT_Plot):
+
+    """
+    A daughter class for making the EFT plots in the style of the LHC experiments
+    """
 
     allowed_experiment_styles = ["ATLAS","ALICE","LHCb2","CMS","ATLASTex"]
 
     def initialise_LHC_plot(self,experiment):
 
-
         assert any([experiment == x for x in self.allowed_experiment_styles]), "Experiment style not defined"
 
-        # hep.style.use(experiment)#, {'xtick.direction': 'out'}])
         if   experiment=="ATLAS": plt.style.use(hep.style.ATLAS) # This is the correct syntax for Pytohn3.6.9 version (mplhep 0.2.8)
         elif experiment=="CMS"  : plt.style.use(hep.style.CMS)
         elif experiment=="ALICE": plt.style.use(hep.style.ALICE)
@@ -124,19 +146,20 @@ class LHC_EFT_Plot(EFT_Plot):
             print("This is not supported because do not have a working TexLive distribution")
             # plt.style.use(hep.style.ATLASTex) # T
 
-
-    def __init__(self,df,orientation,experiment):
-        super().__init__(df,orientation)
+    def __init__(self,df,experiment,**kwargs,):
+        
+        super().__init__(df,**kwargs)
         self.experiment = experiment
 
         self.initialise_LHC_plot(experiment)
 
 
-x = LHC_EFT_Plot(df=df,orientation="hor",experiment="ATLAS")
-plt,fig,ax=x.make_vertical_plot()
-hep.atlas.text("Internal",loc=0)
-plt.show()
-input()
+
+
+
+
+
+
 
 
 
